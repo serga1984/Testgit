d22346641638f1e89e1ba04f7c937f9a7aeb4dd0
User <kiselev.sp@outlook.com>
2022-03-29 23:10:14 +0300

    выполнил правки

--- src/lesson1/Homework1.java
+++ src/lesson1/Homework1.java
@@ -2,6 +2,6 @@ package lesson1;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("Решение первой задачи");
+        System.out.println("Решение первой задачи!");
     }
 }
